5bd4a0994844ccc190d80fa5d7f60d73a1236c5b
Túlio <edd.di@unip.br>
2022-05-21 15:14:16 -0300

    atualização cadastro + avatar

--- LS_v2/src/main.js
+++ LS_v2/src/main.js
@@ -179,14 +179,14 @@ var avatar = document.querySelector('.avatar');
 function ajax() {
 
     // Injeta o html do componente "target".
-    $.get('components/login/login.html', function (response) {
-      $('.login').html(response);
-    });
-
-
-    // $.get('components/cadastro/cadastro.html', function (response) {
-    //   $('.cadastro').html(response);
+    // $.get('components/login/login.html', function (response) {
+    //   $('.login').html(response);
     // });
+
+
+    $.get('components/cadastro/cadastro.html', function (response) {
+      $('.cadastro').html(response);
+    });
 
     // $.get('components/cadastro/cadastro.html', function (response) {
     //   $('.cadastro').html(response);
